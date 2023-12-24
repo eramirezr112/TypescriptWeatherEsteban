8ba0d72c10f50db104edfffea513f1edbd22c233
Esteban Ramirez <esteban.ramirez@5e.cr>
2023-12-24 10:45:24 -0600

    Apply Code Changes - Solution

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@
 import './styles/main.scss';
 
 // Import the API request method
+import { getWeather } from './networking/weather';
 import { buttonClick, getCity, updateInteface } from './dom-manipulation/domManipulation';
 
-// Add an event listener to the button
-
 // Create an async function to call the API method
+export const displayWeather = async () => {
+    const city = getCity();
+    if(city) {
+        const weather = await getWeather(city);
+        updateInteface(weather);
+    } 
+}
+
+// Add an event listener to the button
+if (buttonClick) buttonClick.addEventListener('click', displayWeather);
